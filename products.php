7b30c6be070b5dbe1c9fa531194aa13f565b8c48
BHAGAT VARUN <bhagatvarun580@gmail.com>
2025-04-29 14:01:54 +0530

    Update products.php

--- products.php
+++ products.php
@@ -25,7 +25,7 @@
             ?>
             <div class="container">
                 <div class="jumbotron">
-                    <h1>Welcome to our Projectworlds Store!</h1>
+                    <h1>Welcome to our Maximo Shop !</h1>
                     <p>We have the best cameras, watches and shirts for you. No need to hunt around, we have all in one place.</p>
                 </div>
             </div>
